2c2d36cc6079ee9c6d368aa76f034ad2e14c6aa0
Afeefa <afeefas008@gmail.com>
2026-01-30 00:02:18 +0530

    feat: Register API implementation.

---
+++ backend/backend/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer): # User model serializing.
+    email = serializers.EmailField(required=True)  #making required.  
+
+    class Meta: # which model, and what are the fields
+        model = User
+        fields = ['username','email','password'] #api access for particular fields only
